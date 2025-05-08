f0e2fc1cf3505916a1faed6e64f1f37276826da1
honours <honoursbhadauria@gmail.com>
2025-05-09 01:21:51 +0530

    Add initial Django project structure and requirements

---
+++ py_client /basic.py
@@ -0,0 +1,20 @@
+import requests 
+
+
+endpoint = "https://www.httpbin.org/status/200"
+
+endpoint  = "https://www.httpbin.org"
+
+#learned about endpoint and requests 
+
+get_response = requests.get(endpoint)
+
+
+print(get_response.text) # to print the response code
+ #aplication programming interface4
+
+ # phone ->  camera -> app -> api -> CAMERA 
+
+#  REST API'S ia a web api 
+
+# http request is used in this 
